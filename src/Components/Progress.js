03a1ad96f0877a3bd3a939f442b7306a09f708e4
Lakisha J <lakishajohnson@pursuit.org>
2023-04-22 17:05:00 -0400

    Components dynamic

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
-}
+const Progress = (props) => {
+  // console.log(props)
+  /* props is an object, W/TWO PROPERTIES on it, MIN & max ASSIGNED IN APP.JS*/
+  return (
+  <section className="progress">
+  <h2>
+    Raised <span className="secondary">${props.min}</span> of
+    <span className="secondary"> ${props.max}</span>
+  </h2>
+</section>
+  )
+};
+
+
+export default Progress;
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,21 @@
-export default function RecentDonations() {
-  return null;
-}
+const RecentDonations = (props) => {
+  // console.log(props)
+  /* THIS PROPS IS AN ARRAY OF OBJECTS, THE DONATIONS ARRAY FROM APP.JS */
+  let recent = props.donations.map((donation) => {
+    return (
+      <li>
+        <span>
+          {donation.name} donated ${donation.amount}
+        </span>
+        {donation.caption}
+      </li>
+    );
+  });
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>{recent}</ul>
+    </section>
+  );
+};
+export default RecentDonations;
