7a2008e156ba1ffda6bcb7a831322b565fa1475d
Lakisha J <lakishajohnson@pursuit.org>
2023-04-22 17:02:47 -0400

    Functioning dynamically

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,37 @@
-export default function DonationForm() {
-  return null;
-}
+const DonationForm = (props) => {
+  // console.log(props)
+  /* THIS PROPS IS AN OBJECT W/ONE PROPERTY ON IT. ASSIGNED IN APP.JS */
+  return (
+    <section class="donation">
+      <h3>You could be donation <span class="secondary">#{props.position}!
+      </span></h3>
+      <form>
+        <label>
+          Name<input 
+          id="name" 
+          name="name" 
+          type="text" 
+          placeholder="Your name..." />
+        </label>
+        <label>
+          Caption<input 
+          id="caption" 
+          name="caption" 
+          type="text" 
+          placeholder="Add a brief message..." />
+        </label>
+        <label>
+          Amount<input 
+          id="amount" 
+          name="amount" 
+          type="number" 
+          placeholder="$0" />
+        </label>
+        <button>Donate!</button>
+      </form>
+    </section>
+  );
+};
+
+
+export default DonationForm;
